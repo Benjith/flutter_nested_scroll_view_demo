da95740893b289cae2aef9fcce0991dddfc6de97
Benjith Kizhisseri <mpbenjith@gmail.com>
2020-10-01 12:33:13 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# nested_scroll_view_demo
+
+A new Flutter project demo project using NestedScrollView widget 
+
---
+++ lib/main.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Nested scroll view demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter nested scroll view demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              expandedHeight: 230.0,
+              flexibleSpace: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    width: 80,
+                    color: Colors.primaries[index % Colors.primaries.length],
+                    alignment: Alignment.center,
+                    child: Text(
+                      '$index',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  );
+                },
+                itemCount: 20,
+              ),
+            ),
+            SliverPersistentHeader(
+              pinned: true,
+              delegate: StickyTabBarDelegate(
+                child: TabBar(
+                  labelColor: Colors.black,
+                  controller: this._tabController,
+                  tabs: <Widget>[
+                    Tab(text: 'information'),
+                    Tab(text: 'technology'),
+                  ],
+                ),
+              ),
+            ),
+          ];
+        },
+        body: TabBarView(
+          controller: this._tabController,
+          children: <Widget>[
+            GridView.builder(
+              gridDelegate:
+                  SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  height: 80,
+                  color: Colors.primaries[index % Colors.primaries.length],
+                  alignment: Alignment.center,
+                  child: Text(
+                    '$index',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                );
+              },
+              itemCount: 20,
+            ),
+            ListView.builder(
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  height: 80,
+                  color: Colors.primaries[index % Colors.primaries.length],
+                  alignment: Alignment.center,
+                  child: Text(
+                    '$index',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                );
+              },
+              itemCount: 20,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class StickyTabBarDelegate extends SliverPersistentHeaderDelegate {
+  final TabBar child;
+
+  StickyTabBarDelegate({@required this.child});
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      color: Theme.of(context).backgroundColor,
+      child: this.child,
+    );
+  }
+
+  @override
+  double get maxExtent => this.child.preferredSize.height;
+
+  @override
+  double get minExtent => this.child.preferredSize.height;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
+    return true;
+  }
+}
